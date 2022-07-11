cb78ab50659ded37f97f5c91da9b342390d192dc
Roman <kysilroman99@gmail.com>
2022-07-11 12:32:45 +0200

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# loockup_email_duplications
+Info
+====
+Script searches email duplication entries in csv files of this format:
+```bash
+id,email,is_active,creation_date
+```
+
+Usage: 
+======
+- Put fiels needed to be parsed in the folder ./loockup
+- then run this command:
+```bash
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+import collections
+import csv
+import asyncio
+
+LOOCKUP_DIR = 'loockup'
+
+emails = []
+
+def parsers_run():
+    tasks = []
+
+    for filename in os.listdir(os.getcwd() + '/' + LOOCKUP_DIR):
+        tasks.append(parse_user(filename))
+    
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
+    
+    loop.close()
+    
+    for result in results:
+        emails.extend(result)
+
+async def parse_user(filename):
+    with open(LOOCKUP_DIR + '/' + filename, 'r') as f:
+        reader = csv.reader(f)
+        
+        return [field[1] for field in reader if field[1] != 'email']
+
+def email_duplicates():
+    return [item for item, count in collections.Counter(emails).items() if count > 1]
+
+def grep_duplicates():
+    for email in email_duplicates():
+        print(f'<<<<<< We found duplicates of {email} in these files>>>>>>>\n')
+        subprocess.call(['grep', '-r', email, './' + LOOCKUP_DIR])
+        print("\n>>>>>>>>>>>>>><<<<<<<<<<<<<<\n\n")
+
+def main():
+    parsers_run()
+    grep_duplicates()
+
+
+if __name__ == '__main__':
+    main()
